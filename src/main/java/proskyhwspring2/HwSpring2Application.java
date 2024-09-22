043157d2284dc2c246a96809a172be48de21a352
Dmitryi Kuznetsov <d.w.kuznetsov@yandex.ru>
2024-09-22 12:45:33 +0300

    spring

---
+++ src/main/java/proskyhwspring2/HwSpring2Application.java
@@ -0,0 +1,13 @@
+package proskyhwspring2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwSpring2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwSpring2Application.class, args);
+	}
+
+}
